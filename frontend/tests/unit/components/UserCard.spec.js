9afe8fb03d24a7a62df662670079e8677c948e41
Denny <Nathan.Denny@gov.bc.ca>
2019-10-29 14:52:31 -0700

    Added numeric parameters to template

--- frontend/tests/unit/components/UserCard.spec.js
+++ frontend/tests/unit/components/UserCard.spec.js
@@ -3,32 +3,24 @@ import Vuetify from 'vuetify';
 import Vue from 'vue';
 import Vuex from 'vuex';
 import UserCard from '../../../src/components/UserCard.vue';
-import auth from '@/store/modules/auth';
+import auth from '../../../src/store/modules/auth';
 
 describe('Home.vue', () => {
   let wrapper;
   let store;
-  
+
   beforeEach(() => {
 
     Vue.use(Vuetify);
     Vue.use(Vuex);
 
     store = new Vuex.Store({
-      modules: { auth }
+      modules: {auth}
     });
 
     wrapper = shallowMount(UserCard, {
       Vue: Vue,
-      store,
-      propsData: {
-        userInfo: {
-          displayName: 'Nathan Denny',
-          _json: {
-            preferred_username: 'fake@setup'
-          }
-        }
-      }
+      store
     });
   });
 
