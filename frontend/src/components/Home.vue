360cfaba64b9d70a180d6dcb51fd10affb9a3d1c
NDENNY <nathan.denny@gov.bc.ca>
2019-11-28 11:41:58 -0800

    Removed logging in endpoint

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -20,7 +20,6 @@
     </article>
   </v-container>
 
-<!--
   <v-container fluid class="full-height" v-else-if="isAuthenticated && dataReady && userInfo.pen">
     <article id="pen-display-container" class="top-banner full-height">
       <v-row align="center" justify="center">
@@ -28,8 +27,7 @@
       </v-row>
     </article>
   </v-container>
--->
-
+  
   <v-container fluid class="full-height" v-else-if="isAuthenticated && dataReady">
     <!-- pen request form -->
     <article id="request-form-container" class="top-banner full-height">
