d289aa20a840a007f63c93f2c5206bb58c4f851b
NDENNY <nathan.denny@gov.bc.ca>
2019-12-12 09:46:14 -0800

    Added code table endpoint

--- backend/src/routes/pen.js
+++ backend/src/routes/pen.js
@@ -32,4 +32,20 @@ router.post('/request', passport.authenticate('jwt', { session: false }),
   }
 );
 
+router.get('/gender_codes', passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try{
+      const response = await axios.get(config.get('codeTable:genderEndpoint'));
+      if(response.status !== 200){
+        return res.status(response.status).json({
+          message: 'API Post error'
+        });
+      }
+      return res.status(200).json(res.body);
+    } catch (e) {
+      return res.status(500).json(e);
+    }
+  }
+);
+
 module.exports = router;
