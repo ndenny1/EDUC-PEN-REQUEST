4752c5044b0f816a33428acc96ccb16d77d175ac
om <omprakashmishra3978@gmail.com>
2020-02-19 13:13:32 -0800

    code changes for pen-120 making the app responsive

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -4,30 +4,30 @@
   <v-container fluid v-if="!isAuthenticated && !isLoading">
     <!-- login article -->
     <article name="login-banner" class="top-banner">
-      <v-row align="center" justify="center">
-        <Login></Login>
-      </v-row>
+        <v-row align="center" justify="center" style="margin-right: 0;margin-left: 0">
+          <Login></Login>
+        </v-row>
     </article>
     <article id="info-section" class="infoTab">
-      <v-row align="center" justify="center">
-        <Info></Info>
-      </v-row>
+        <v-row align="center" justify="center" style="margin-right: 0;margin-left: 0">
+          <Info></Info>
+        </v-row>
     </article>
     <article id="info-cards" class="bottomContainer">
-      <v-row align="center" justify="center">
-        <LoginCards></LoginCards>
-      </v-row>
-    </article>
+          <v-row style="margin-right: 0;margin-left: 0">
+            <LoginCards></LoginCards>
+          </v-row>
+        </article>
   </v-container>
 
   <v-container fluid class="full-height" v-else-if="isLoading">
     <article id="pen-display-container" class="top-banner full-height">
       <v-row align="center" justify="center">
         <v-progress-circular
-          :size="70"
-          :width="7"
-          color="primary"
-          indeterminate
+                :size="70"
+                :width="7"
+                color="primary"
+                indeterminate
         ></v-progress-circular>
       </v-row>
     </article>
@@ -35,7 +35,7 @@
 
   <v-container fluid class="full-height" v-else-if="isAuthenticated && hasPen">
     <article id="pen-display-container" class="top-banner full-height">
-      <v-row align="center" justify="center">
+      <v-row align="center" justify="center" style="width: 1vw;margin-right: 0;margin-left: 0;margin-bottom: 5rem;">
         <PenDisplay></PenDisplay>
       </v-row>
     </article>
@@ -43,19 +43,23 @@
 
   <v-container fluid class="full-height" v-else-if="isAuthenticated && hasPenRequest">
     <article id="request-display-container" class="top-banner full-height">
-      <v-row align="center" justify="center"> 
-        <MoreInfoForm v-if="requireMoreInfo"></MoreInfoForm>
-        <RequestDisplay v-else></RequestDisplay>
-     </v-row>
+        <v-row align="center" justify="center" style="width: 1vw;margin-right: 0;margin-left: 0;margin-bottom: 5rem;">
+          <v-col xs="10" sm="10" md="10" lg="10" xl="10">
+            <MoreInfoForm v-if="requireMoreInfo"></MoreInfoForm>
+            <RequestDisplay v-else></RequestDisplay>
+          </v-col>
+        </v-row>
     </article>
   </v-container>
 
   <v-container fluid class="full-height" v-else-if="isAuthenticated && !hasPen && !hasPenRequest">
     <!-- pen request form -->
     <article id="request-form-container" class="top-banner full-height">
-      <v-row align="center" justify="center">
-        <RequestForm></RequestForm>
-      </v-row>
+        <v-row align="center" justify="center" style="width: 1vw;margin-right: 0;margin-left: 0;margin-bottom: 5rem;">
+          <v-col xs="10" sm="10" md="10" lg="10" xl="10">
+            <RequestForm></RequestForm>
+          </v-col>
+        </v-row>
     </article>
   </v-container>
 
@@ -99,8 +103,8 @@ export default {
       return !!this.userInfo && !!this.userInfo.penRequest;
     },
     requireMoreInfo() {
-      return this.hasPenRequest && 
-        this.userInfo.penRequest.penRequestStatusCode == PenRequestStatuses.RETURNED;
+      return this.hasPenRequest &&
+            this.userInfo.penRequest.penRequestStatusCode === PenRequestStatuses.RETURNED;
     }
   },
   methods: {
@@ -118,25 +122,25 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.container{
-  padding: 0px;
-  padding-bottom: 50px;
-}
-.top-banner{
-  background-color: aliceblue;
-  min-height: 500px;
-  background-size: cover;
-  align-items: center;
-  display: flex;
-}
-.full-height{
-  height: 100%;
-}
-.infoTab{
-  padding: 10px 0px;
-  background-color: #fafafa
-}
-.bottomContainer{
-  padding-bottom: 30px
-}
+  .container{
+    padding: 0px;
+    padding-bottom: 50px;
+  }
+  .top-banner{
+    background-color: aliceblue;
+    background-size: cover;
+    align-items: center;
+    display: flex;
+  }
+  .full-height{
+    height: 100%;
+  }
+  .infoTab{
+    padding: 10px 0px;
+    background-color: #fafafa
+  }
+  .bottomContainer{
+    padding-bottom: 30px
+  }
 </style>
+
--- frontend/src/components/LoginCards.vue
+++ frontend/src/components/LoginCards.vue
@@ -1,26 +1,26 @@
 <template>
-    <v-row class="centerInfo" align="center" justify="center">
-        <v-col md="auto" height=100% class="half-card">
-            <v-card outlined height='170px' dark color="#003366">
-                <v-card-title> <v-icon>$login</v-icon> Easy Log In</v-card-title>
-                <v-card-text>
-                    Log In using a Basic BCeID or B.C. Services Card.
+    <v-container class="container-login-card">
+        <v-col xs="8" sm="8" md="8" lg="8" xl="8" offset-xs="2" offset-sm="2" offset-md="2" offset-lg="2" offset-xl="2">
+            <v-card outlined  dark color="#003366">
+                <v-card-title> <v-icon>$login</v-icon><span class="login-cards-display-header">Easy Log In</span> </v-card-title>
+                <v-card-text><span class="login-cards-display-text">
+                    Log In using a Basic BCeID or B.C. Services Card.</span>
                 </v-card-text>
-                <v-card-actions class="card-actions">
-                    <v-btn width="50%" href="https://www.bceid.ca/" outlined>About BCeID</v-btn>
-                    <v-btn width="50%" href="https://www2.gov.bc.ca/gov/content/governments/government-id/bc-services-card" outlined>About B.C. Services Card</v-btn>
+                <v-card-actions>
+                    <v-btn width="48%" href="https://www.bceid.ca/" outlined><span class="login-cards-display-text">About BCeID</span></v-btn>
+                    <v-btn width="50%" href="https://www2.gov.bc.ca/gov/content/governments/government-id/bc-services-card" outlined><span class="login-cards-display-text-sc">About B.C. Services Card</span></v-btn>
                 </v-card-actions>
             </v-card>
         </v-col>
-        <v-col md="auto" class="half-card">
-            <v-card outlined height='170px' dark color="#6c757d">
-                <v-card-title> <v-icon>$fast</v-icon>Fast Results</v-card-title>
-                <v-card-text>
-                    The {{ appTitle }} app instantly returns your PEN if it can confidently identify the user. If you can not be instantly identified, you will recieve an email once manual identification has been completed.
-                </v-card-text>
+        <v-col xs="8" sm="8" md="8" lg="8" xl="8"  offset-xs="2" offset-sm="2" offset-md="2" offset-lg="2" offset-xl="2">
+            <v-card outlined  dark color="#6c757d">
+                <v-card-title> <v-icon>$fast</v-icon><span class="login-cards-display-header">Fast Results</span></v-card-title>
+                <v-card-text><span class="login-cards-display-text">
+                    The {{ appTitle }} app instantly returns your PEN if it can confidently identify the user. If you can not be instantly identified, you will receive an email once manual identification has been completed.
+                </span></v-card-text>
             </v-card>
         </v-col>
-    </v-row>
+    </v-container>
 </template>
 
 <script>
@@ -45,10 +45,7 @@ export default {
     bottom: 10px;
     position: absolute;
 }
-.centerInfo{
-    max-width: 900px;
-    width: 55%;
-}
+
 .half-card{
     width: 48%;
 }
@@ -62,5 +59,60 @@ export default {
     text-transform: none;
     overflow: hidden;
     font-size: 0.8em
+}
+.container-login-card{
+    padding-bottom: 5vh;
+}
+.login-cards-display-header{
+    font-size: 2vh;
+}
+.login-cards-display-text{
+    font-size: 1.8vh;
+}
+.login-cards-display-text-sc{
+    font-size: 1.7vh;
+}
+@media screen and (min-width: 601px) and (max-width:900px) {
+    .container-login-card{
+        padding-bottom: 4vh !important;
+    }
+    .login-cards-display-header{
+        font-size: 2vh;
+    }
+    .login-cards-display-text{
+        font-size: 1.8vh;
+    }
+    .login-cards-display-text-sc{
+        font-size: 1.6vh;
+    }
+}
+@media screen and (min-width: 301px) and (max-width: 600px){
+    .container-login-card{
+        padding-bottom: 18vh !important;
+    }
+    .login-cards-display-header{
+        font-size: 3vh;
+    }
+    .login-cards-display-text{
+        font-size: 1.5vh;
+    }
+    .login-cards-display-text-sc{
+        font-size: 1.4vh;
+    }
+}
+
+@media screen and (max-width: 300px) {
+    .container-login-card{
+        padding-bottom: 40% !important;
+    }
+    .login-cards-display-header{
+        font-size: 2vh;
+    }
+    .login-cards-display-text{
+        font-size: 1.2vh;
+    }
+    .login-cards-display-text-sc{
+        font-size: 1vh;
+    }
 }
 </style>
